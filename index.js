9a6c5c4d26fdc7c3bf98de0354e7d1857b931408
Joponspolo <159774189+jonathanPons@users.noreply.github.com>
2025-12-29 07:53:29 +0100

    travail Local Storage

--- index.js
+++ index.js
@@ -4,6 +4,48 @@ const list = document.getElementById("tacheAdd");
 const tacheElement = document.getElementById("tacheElement");
 input.value = "";
 let todos = [];
+
+//fonction qui sauvegarde la todo
+function saveTodos() {
+  const todoString = JSON.stringify(todos);
+  localStorage.setItem("todos", todoString);
+}
+
+// fonctionqui cahrge la todo
+
+function chargeTodos() {
+  const todoString = localStorage.getItem("todos");
+  if (todoString === null) {
+    return;
+  }
+  todos = JSON.parse(todoString);
+  todos.forEach(function (tacheTexte) {
+    const tacheArray = document.createElement("li");
+    tacheArray.textContent = tacheTexte;
+    list.appendChild(tacheArray);
+    const check = document.createElement("i");
+    check.classList.add("fa-solid", "fa-check");
+    tacheArray.appendChild(check);
+    const rubish = document.createElement("i");
+    rubish.classList.add("fa-solid", "fa-trash-can");
+    console.log(rubish);
+    tacheArray.appendChild(rubish);
+    rubish.addEventListener("click", () => {
+      tacheArray.remove();
+    });
+
+    check.addEventListener("click", () => {
+      check.classList.toggle("activ");
+      if (check.classList.contains("activ")) {
+        tacheArray.style = "text-decoration: line-through;";
+      } else if (!check.classList.contains("activ")) {
+        tacheArray.style = "text-decoration: none;";
+      }
+    });
+    input.value = "";
+  });
+}
+
 function addtodo() {
   if (input.value === "") {
     alert("Veuillez rentrer une tache");
@@ -52,9 +94,38 @@ input.addEventListener("keypress", (e) => {
 
 //A. Sauvegarder les données
 //localStorage.setItem("clé", "valeur")
+
 //B. Récupérer une donnée
 // localStorage.getItem("clé")
+
 //C.Supprimer une donnée
 //localStorage.removeItem("clé")
+
 //Tout Supprimer
 //localStorage.clear()
+
+//localStorage.setItem("prenom", "Jonathan");
+//const prenom = localStorage.getItem("prenom");
+//console.log(prenom);
+//const sports = ["Running", "JJB", "Natation"];
+//localStorage.setItem("sports", sports);
+//const sportsSaved = localStorage.getItem("sports");
+//console.log(sportsSaved); // Regarde ce que ça donne
+//console.log(typeof sportsSaved); // C'est quel type ?
+
+//Exemple bonne methode
+
+const sports = ["Running", "JJB", "Natation"];
+//Convertir en string
+const SportsStrings = JSON.stringify(sports);
+//Sauvegarder
+localStorage.setItem("sports", SportsStrings);
+
+// Récuperer puis convertir string en array
+
+const sportsSaved = localStorage.getItem("sports");
+const sportsArray = JSON.parse(sportsSaved);
+
+console.log(sportsArray);
+console.log(typeof sportsArray);
+console.log(Array.isArray(sportsArray));
